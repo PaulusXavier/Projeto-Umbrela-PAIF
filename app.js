9c115d4a2ff873584231f2cae74539cacb13ed5a
Paulo Xavier <paulo.ae18@gmail.com>
2026-07-28 18:36:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -126,8 +126,6 @@ const state = {
   railOpen: false
 };
 
-const FIRESTORE_COLLECTION = "planos_acompanhamento_familiar";
-
 function uid() {
   return "paf_" + Date.now().toString(36) + "_" + Math.random().toString(36).slice(2, 9);
 }
